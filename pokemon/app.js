f8d4a89e1bed942bbeee991983398a9e73bbaa8f
IDStack <idstack@outlook.co.id>
2022-12-27 18:30:34 +0700

    update source code

--- pokemon/app.js
+++ pokemon/app.js
@@ -1,19 +1,20 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
 const container = document.querySelector('#container');
-const imgURL =
+const baseImgURL =
 	'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 for (let i = 1; i <= 500; i++) {
-	const pokemon = document.createElement('div');
-	pokemon.classList.add('pokemon');
+	const pokeBall = document.createElement('div');
+	pokeBall.classList.add('pokemon');
 
 	const imgPokemon = document.createElement('img');
-	imgPokemon.src = `${imgURL}${i}.png`;
+	imgPokemon.src = `${baseImgURL}${i}.png`;
 
 	const label = document.createElement('span');
 	label.innerText = `#${i}`;
 
-	pokemon.appendChild(imgPokemon);
-	pokemon.appendChild(label);
-	container.appendChild(pokemon);
+	pokeBall.appendChild(imgPokemon);
+	pokeBall.appendChild(label);
+	container.appendChild(pokeBall);
 }
